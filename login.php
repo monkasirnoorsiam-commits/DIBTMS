4dc208b5cc72c0ec76051863ead575bcc6b2fa53
MD. Fahim Abdullah Kayfe Khan <fahim.abdullah.kayfe@g.bracu.ac.bd>
2025-12-27 04:20:30 +0600

    change in footer
    Added styling to body and footer for better layout.

--- login.php
+++ login.php
@@ -9,8 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DIBTMS Login</title>
 </head>
-<body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+<body style="min-height:100vh; display:flex; flex-direction:column;">
+ <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
         <h2>Log into your account</h2>
         Email or phone number:<br>
         <input type="text" name="input"><br>
@@ -18,31 +18,34 @@
         <input type="password" name="password"><br>
         <input type="submit" name="submit" value="login"><br>
     </form>
-</body>
-</html>
+
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $input = $_POST["input"];
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
         if(empty($input)){
-            echo"Please enter a email or a phone number";
+            echo "Please enter a email or a phone number";
         }
         elseif(empty($password)){
-            echo"Please enter a password";
+            echo "Please enter a password";
         }
         else{
             $hash = password_hash($password, PASSWORD_DEFAULT);
+
             if (filter_var($input, FILTER_VALIDATE_EMAIL)) {
                 $sql = "SELECT * FROM users where email = '$input'";
-            } 
-            else {
+            } else {
                 $sql = "SELECT * FROM users where phone_no = '$input'";
             }
+
             $result = mysqli_query($conn, $sql);
+
             if(mysqli_num_rows($result) > 0){
                 if(password_verify($password, $hash)){
                     $row = mysqli_fetch_assoc($result);
                     $_SESSION["id"] = $row["id"];
+
                     if(substr($_SESSION["id"], 0, 1) == 1){
                         header("Location: admin.php");
                     }
@@ -61,6 +64,9 @@
     }
     mysqli_close($conn);
 ?>
-<?php 
-    include("footer.html");
-?>
+<div style="margin-top:auto;">
+        <?php include("footer.html"); ?>
+    </div>
+
+</body>
+</html>
