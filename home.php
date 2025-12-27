97e90fe90cdb022ee2171b83942373e45b723d92
MD. Fahim Abdullah Kayfe Khan <fahim.abdullah.kayfe@g.bracu.ac.bd>
2025-12-27 08:38:47 +0600

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,9 @@
 <?php
-// hope.php
-include("header.php"); // your header with logo and nav
+include("header.php");
 ?>
 
 <div class="main-content">
     <h1>Welcome to DIBTMS</h1>
-    
 </div>
 
-<?php include("footer.html"); // your Contact Us footer ?>
+<?php include("footer.html"); ?>
