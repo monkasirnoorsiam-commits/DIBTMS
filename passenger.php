aa34e8145f966beaf46b0b41bfae46f9f7e3d133
MD. Fahim Abdullah Kayfe Khan <fahim.abdullah.kayfe@g.bracu.ac.bd>
2026-01-01 16:35:21 +0600

    Update passenger.php

--- passenger.php
+++ passenger.php
@@ -16,14 +16,20 @@
 
 <body style="min-height:100vh; display:flex; flex-direction:column;">
 
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
-        <h2>Actions: </h2>
-        <input type="submit" name="book" value="Book a ride"><br>
-        <input type="submit" name="payment" value="Set payment options"><br>
-        <input type="submit" name="review" value="Give reviews"><br>
-        <input type="submit" name="history" value="Your Ride History"><br>
-        <input type="submit" name="account" value="Account info"><br>
-    </form>
+   <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" class="action-form">
+    <h2>Hop In & Go! Your Journey Awaits </h2>
+
+    <div class="buttons-container">
+        <input type="submit" name="book" value="Book a ride" class="action-btn">
+        <input type="submit" name="payment" value="Set payment options" class="action-btn">
+        <input type="submit" name="review" value="Give reviews" class="action-btn">
+        <input type="submit" name="history" value="Your Ride History" class="action-btn">
+        <input type="submit" name="account" value="Account info" class="action-btn">
+    </div>
+</form>
+
+
+
 
 <?php
     if(isset($_POST["book"])){
